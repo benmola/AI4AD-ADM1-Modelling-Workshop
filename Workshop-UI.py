14171e96d5e44b1954313e717ea76ae252b4628d
Benaissa DEKHICI <b.dekhici@surrey.ac.uk>
2025-09-14 11:06:45 +0100

    Update Workshop-UI.py

--- Workshop-UI.py
+++ Workshop-UI.py
@@ -195,6 +195,3 @@ reset_button.on_click(on_reset_clicked)
 # # Display interface
 # #display(VBox([HBox([feedstock_acc, process_acc]), HBox([run_button, reset_button]), output]))
 display(HBox([feedstock_acc, process_acc]), HBox([run_button, reset_button]), output)
-
-
-
